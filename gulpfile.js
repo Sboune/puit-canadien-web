5fb940c5ef362fb8d3b67eeb306d4664989d085a
Lassier Antoine <toinousp@gmail.com>
2015-01-15 09:09:44 +0100

    Mise en place de bower, gulp et sass
    Add: fichiers bower, pour installer des librairies tierces dans `assets/vendor`
    Fix: modification des liens dans le header pour prendre en compte le changement de path des fichiers des librairies tierces
    Add: fichiers sass dans `assets/sass`
    - fichier mains.scss, _normalize.scss, _base.scss, _debug.scss, _mixins.scss, _skeleton.scss, _variables.scss
    Add: ajout des fichiers pour utiliser gulp : `gulpfile.js`
    Fix: ajout de `.sass-cache` dans le `.gitignore`

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,48 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+
+var paths = {
+
+    styles: {
+        src: './assets/sass',
+        files: './assets/sass/main.scss',
+        dest: './assets/css'
+    }
+
+}
+
+// A display error function, to format and make custom errors more uniform
+// Could be combined with gulp-util or npm colors for nicer output
+var displayError = function(error) {
+
+    var errorString = '[' + error.plugin + ']';
+    errorString += ' ' + error.message.replace("\n",'');
+
+    if(error.fileName)
+        errorString += ' in ' + error.fileName;
+
+    if(error.lineNumber)
+        errorString += ' on line ' + error.lineNumber;
+
+    console.error(errorString);
+}
+
+
+gulp.task('sass', function (){
+    gulp.src(paths.styles.files)
+    .pipe(sass({outputStyle: 'compact', ncludePaths : [paths.styles.src]}))
+    .on('error', function(err){displayError(err);})
+    .pipe(prefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest(paths.styles.dest))
+});
+
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch(paths.styles.files, ['sass'])
+    .on('change', function(evt) {
+        console.log(
+            '[watcher] File ' + evt.path.replace(/.*(?=sass)/,'') + ' was ' + evt.type + ', compiling...'
+        );
+    });
+});
