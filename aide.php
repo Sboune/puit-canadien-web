f4b581cb056288c3178e334518cb23e2260051d9
Lassier Antoine <toinousp@gmail.com>
2015-01-20 15:52:17 +0100

    Mise en page de l'aide
    Add: Ajout d'un fichier scrollspy.js pour que le menu fixe suivent l'avancement dans la page
    Change: Modification du contenu de la page aide (contenu fictif pour l'instant)
    Fix: _skeleton utilise la variable $larger-than-tablet
    Change: Modification de _base.scss pour rajouter les styles de la sub-nav

--- aide.php
+++ aide.php
@@ -3,17 +3,24 @@
 <html lang="fr">
 <head>
   <?php include('includes/layout/head.php'); ?>
-  <link rel="stylesheet" type="text/css" href="assets/css/doc.css">
+  <script type="text/javascript" src="assets/js/scrollspy.js"></script>
 </head>
 <body>
 
   <?php include('includes/layout/header.php'); ?>
 
-  <div id="contenu">
-    <?php include('includes/layout/doc.html'); ?>
+  <div class="container">
+    <div class="wrapper">
+      <?php include('includes/layout/doc.html'); ?>
+    </div>
   </div>
 
   <?php include('includes/layout/footer.php'); ?>
-
+  <script>
+    $('body').scrollspy({
+      target: '.bs-docs-sidebar',
+      offset: 180
+    });
+  </script>
 </body>
 </html>
